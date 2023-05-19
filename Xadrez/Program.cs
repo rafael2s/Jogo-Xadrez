a2e71cd5537c2a76cdeb10ada8d88dae16a6e549
Rafael Souza <rafael_popo@hotmail.com>
2023-05-19 16:55:48 -0300

    Posicionamento pecas no tabuleiro

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using tabuleiro;
+using xadrez;
 
 namespace Xadrez
 {
@@ -14,6 +15,10 @@ namespace Xadrez
             //Console.WriteLine("Posição: " + P);
 
             Tabuleiro tab = new Tabuleiro(8, 8);
+
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
 
             Tela.imprimirTabuleiro(tab);
 
--- Xadrez/Tabuleiro/Peca.cs
+++ Xadrez/Tabuleiro/Peca.cs
@@ -7,12 +7,13 @@
         public int qtdMovimentos { get; protected set; }
         public Tabuleiro tab { get; protected set; }
 
-        public Peca(Posicao posicao, Cor cor, int qtdMovimentos, Tabuleiro tab)
+        public Peca(Tabuleiro tab, Cor cor)
         {
-            this.posicao = posicao;
+            this.posicao = null;
             this.cor = cor;
             this.qtdMovimentos = 0;
             this.tab = tab;
         }
+
     }
 }
