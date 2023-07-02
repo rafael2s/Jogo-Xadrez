cab31a664063dbc51e6412bdb3d7112fc25b8c21
Rafael Souza <rafael_popo@hotmail.com>
2023-07-02 16:44:00 -0300

    Melhorando a impressão do tabuleito

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -8,19 +8,16 @@ namespace Xadrez
     {
         static void Main(string[] args)
         {
-            //Posicao P;
-
-            //P = new Posicao(3, 4);
-
-            //Console.WriteLine("Posição: " + P);
 
             try
             {
                 Tabuleiro tab = new Tabuleiro(8, 8);
 
                 tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 9));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
                 tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
+
+                tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(3, 5));
 
                 Tela.imprimirTabuleiro(tab);
             }
