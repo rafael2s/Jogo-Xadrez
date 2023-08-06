31e664c8c4f0da53e966a4862ca4a3d28c8dabfc
Rafael Souza <rafael_popo@hotmail.com>
2023-08-06 00:01:22 -0300

    Fazendo os primeiros testes de movimentaçao

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -13,6 +13,18 @@ namespace Xadrez
             {
                 PartidaDeXadrez partida = new PartidaDeXadrez();
 
+                while (!partida.terminada) {
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab);
+
+                    Console.WriteLine();
+                    Console.Write("Origem: ");
+                    Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                    Console.Write("Destino: ");
+                    Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+
+                    partida.executaMovimento(origem, destino);
+                }
 
 
                 Tela.imprimirTabuleiro(partida.tab);
